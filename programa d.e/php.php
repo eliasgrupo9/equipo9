b76ef778c1945c4283dd1422a398723958e76ed3
eliasgrupo9 <88780072+eliasgrupo9@users.noreply.github.com>
2021-08-25 08:56:12 -0500

    Add files via upload

---
+++ programa d.e/php.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Primera pagina php</title>
+   <link rel="stylesheet" href="css/Registro.css">
+   <script src ="Js/Registro.js"></script>
+</head>
+<body>
+
+<?php
+print "esta es la primera página...<br><br>";
+print "Bienvenidos al curso de PHP<BR>";
+print"ESTUDIANTES";
+print"I.E FRANCISCO ANTONIO ZEA-PROMO 2021**11-1<BR>";
+print"Les deseamos un excelente curso...!!!";
+echo "<BR><BR>HOLA MUNDO.....!!!"
+
+/*Definicion De Variables */
+$Nombre + 1234;
+$Apellido + "lopez";
+$resultado =Nombre + Apellido;
+echo $resultado;
+
+?>
+</body>
+
+</html>
